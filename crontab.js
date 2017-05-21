38b68db78b1c94b95c68341a3e896b0abf125cf9
Stéphane Gully <stephane.gully@gmail.com>
2017-05-21 20:15:45 +0200

    add date for better logging

--- crontab.js
+++ crontab.js
@@ -5,9 +5,9 @@ var config   = require('./.local-web-server.json');
 var j = schedule.scheduleJob(config.crontab.when, runCrontabStuff);
 
 function runCrontabStuff() {
-  console.log('ezmaster-webserver: running cron tasks');
+  console.log(new Date() + ' - ezmaster-webserver: running cron tasks');
   config.crontab.commands.forEach(function (cmd) {
-    console.log('ezmaster-webserver: running ' + cmd);
+    console.log(new Date() + ' - ezmaster-webserver: running ' + cmd);
     shell.exec(cmd);
   });
 }
