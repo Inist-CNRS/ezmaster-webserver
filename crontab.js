78a2fb9e5498292db19c42bcaa8f58d24164f966
Stéphane Gully <stephane.gully@gmail.com>
2017-07-27 18:12:16 +0200

    add the silent option to hide the cron logs from stdout

--- crontab.js
+++ crontab.js
@@ -5,10 +5,14 @@ var config   = require('./.local-web-server.json');
 var j = schedule.scheduleJob(config.crontab.when, runCrontabStuff);
 
 function runCrontabStuff() {
-  console.log(new Date() + ' - ezmaster-webserver: running cron tasks');
+  if (!config.crontab.options.silent) {
+    console.log(new Date() + ' - ezmaster-webserver: running cron tasks');
+  }
   config.crontab.commands.forEach(function (cmd) {
-    console.log(new Date() + ' - ezmaster-webserver: running ' + cmd);
-    shell.exec(cmd);
+    if (!config.crontab.options.silent) {
+      console.log(new Date() + ' - ezmaster-webserver: running ' + cmd);
+    }
+    shell.exec(cmd, { silent: config.crontab.options.silent });
   });
 }
 runCrontabStuff();
